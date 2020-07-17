acc2ef7becefab5ecbd11621beefdcfe800df1e0
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2020-07-17 14:55:39 +0900

    update

--- Game/main.cpp
+++ Game/main.cpp
@@ -10,46 +10,22 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	InitEngineParameter initparam;
 	
 	//TODO フルスク対応
-	initparam.windowStyle = WS_POPUP | WS_BORDER ;		//ウィンドウスタイル
-	initparam.screenWidth = 640;			//ウィンドウの幅
-	initparam.screenHeight = 640;			//ウィンドウの高さ
-	initparam.frameBufferWidth = 640;		//フレームバッファの幅。これが内部解像度。
-	initparam.frameBufferHeight = 640;		//フレームバッファの高さ。これが内部解像度。
-	
-	initparam.frameBufferWidth3D = 640;// 1080;	//3D描画の解像度(幅)
-	initparam.frameBufferHeight3D = 640;//1080;	//3D描画の解像度(高さ)
-	//initparam.isAntiAliasing = false;		//アンチエイリアス無効
+	//initparam.windowStyle = WS_POPUP | WS_BORDER ;		//ウィンドウスタイル
 
-	initparam.SDUnityChanScale = 3.5f;		//距離スケール設定
-	//0.15f
+	//解像度
+	//initparam.screenWidth = initparam.frameBufferWidth = initparam.frameBufferWidth3D = 1920;
+	//initparam.screenHeight = initparam.frameBufferHeight = initparam.frameBufferHeight3D = 1080;
 
-	initparam.isEnableDOF = true;
-	initparam.standardFps = 60;
-	initparam.limitFps = -1;
-	//initparam.shadowMapSetting = enON;
+	initparam.useFpsLimiter = false;
+	initparam.useVSync = false;
+	initparam.isWindowMode = true;
 
 	//シェーダー
 	//※デバッグ用
 	initparam.isShaderPathReplaceForEngineFilePath = true;
-	//initparam.SSAOBufferScale = 1.0f;
 	
 	//エンジン初期化
 	GetEngine().InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, "Obosh", initparam);	
-
-	GetEngine().SetUseFpsLimiter(false);
-
-	//DOF設定
-	GetGraphicsEngine().GetDOFRender().SetFocus(0.0f);
-	GetGraphicsEngine().GetDOFRender().SetFocusArea(0.0f);
-	GetGraphicsEngine().GetDOFRender().SetNear(0.0f);
-	GetGraphicsEngine().GetDOFRender().SetFar(30000.0f);
-
-	//歪曲収差設定
-	SetLensDistortionScale(5.0f);
-
-	//物理ワールド設定
-	GetPhysicsWorld().SetGravity({ 0.0f,-800.0f,0.0f });//重力
-	//GetPhysicsWorld().GetSoftBodyWorldInfo()->air_density = 1.2f*10.0f;
 	
 	//判定表示
 	//SetPhysicsDebugDrawMode(btIDebugDraw::DBG_DrawWireframe);
@@ -88,6 +64,11 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
 	class TestGOFactry :public IGameObject {
 	public:
+		bool Start()override {
+			//m_sprite.Init(L"smoke.png"); 
+			//m_spriteDDS.Init(L"smoke.dds");
+			return true;
+		}
 		void Update()override {
 			if (GetKeyDown(VK_SPACE)) {
 				for (int i = 0; i < 10000; i++) {
@@ -109,15 +90,22 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 			num, num2);
 			//std::to_chars(std::begin(stg),std::end(stg),num);
 			font.Draw(stg, 0.5f);
+
+			m_sprite.Draw({ 0.0f,0.0f }, 0.05f, 0.0f, 0.0f, { 1.0f,1.0f ,1.0f ,1.0f });
+			m_spriteDDS.Draw({ 0.8f,0.0f }, 0.05f, 0.0f, 0.0f, { 1.0f,1.0f ,1.0f ,1.0f });
 		}
 
 	private:
 		int num = 0, num2 = 0;
 		CFont font;
 		//TestGO obj;
+
+		//GetSpriteBatchPMAとGetSpriteBatchの使い方逆?
+		//逆じゃないっぽい
+		CSprite m_sprite, m_spriteDDS;
 	};
 
-	TestGOFactry test;
+	//TestGOFactry test;
 
 	//ゲームループ。
 	GetEngine().RunGameLoop();
